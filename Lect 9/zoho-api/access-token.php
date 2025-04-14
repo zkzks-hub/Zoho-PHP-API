a0696d57277e47822fe8f17c8a8637fe0b2f756c
Mohamed Saad <67190055+zkzks-hub@users.noreply.github.com>
2025-04-14 11:28:57 +0200

    Update access-token.php

--- Lect 9/zoho-api/access-token.php
+++ Lect 9/zoho-api/access-token.php
@@ -1,9 +1,9 @@
 <?php
 function generateAccessToken(){
 $post=array(
-	'refresh_token' => '1000.fe7c22d82877997d6d7ca60bfda6e253.5ad7cb35de57743d58bb716374146f75',
-	'client_id' => '1000.GJID0QNTYO9JNY8Q318PDVBYNCKMGI',
-	'client_secret' => '0f7011664ed2569194f62ec95d838599955faa4dc6',
+	'refresh_token' => 'Replace With Your REFRESH_TOKEN',
+	'client_id' => 'Replace With Your CLIENT_ID',
+	'client_secret' => 'Replace With Your CLIENT_SECRET',
 	'grant_type' => 'refresh_token',
 );
 $ch=curl_init();
